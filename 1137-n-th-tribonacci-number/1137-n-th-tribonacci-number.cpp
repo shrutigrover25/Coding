e30448f75176229899ef357d1f3a06785fa960d7
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2022-04-10 23:17:06 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.04%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        
+        int f[n+3];
+        
+        f[0]=0;
+        f[1]=1;
+        f[2]=1;
+        
+        
+        for(int i=3;i<=n;i++)
+        {
+            f[i]=f[i-1]+f[i-2]+f[i-3];
+        }
+        
+        return f[n];
+        
+    }
+};
