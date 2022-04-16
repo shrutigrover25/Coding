df750c25262198d0139536847e7b5be63752fe6f
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2022-04-17 01:46:37 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code hereme
+      int dp[W+1];
+      memset(dp, 0, sizeof dp);
+ 
+    // Fill dp[] using above recursive formula
+    for (int i=0; i<=W; i++)
+      for (int j=0; j<N; j++)
+         if (wt[j] <= i)
+            dp[i] = max(dp[i], dp[i-wt[j]] + val[j]);
+ 
+    return dp[W];
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, W;
+        cin>>N>>W;
+        int val[N], wt[N];
+        for(int i = 0;i < N;i++)
+            cin>>val[i];
+        for(int i = 0;i < N;i++)
+            cin>>wt[i];
+        
+        Solution ob;
+        cout<<ob.knapSack(N, W, val, wt)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
