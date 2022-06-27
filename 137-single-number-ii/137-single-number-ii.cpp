976e233cdc46efaba5ce3bd3d0cd976ed54a1ac8
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2022-06-28 01:14:40 +0530

    Time: 23 ms (9.37%), Space: 10.3 MB (12.41%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        map<int,int>mp;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        
+        int ans;
+        
+        for(auto it=mp.begin();it!=mp.end();it++)
+        {
+            if(it->second==1)
+            {
+                ans=it->first;
+                break;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
