8d0a1acb4e087b9772ba9627be05163a9cd7c9fb
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2022-07-13 02:08:01 +0530

    Time: 4 ms (22.52%), Space: 5.8 MB (83.64%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     
-int steps(int n, vector<int>&dp)
+int fun(int n, int dp[])
 {
-   if(n==0)
-   {
-       return dp[n]=1;
-   }
-    
-   if(n==1)
-   {
-       return dp[n]=1;
-   }
+    if(n<=1)
+    {
+        return dp[n]=1;
+    }
     
     if(dp[n]!=-1)
     {
         return dp[n];
     }
     
-    return dp[n]=steps(n-1,dp)+steps(n-2,dp);
+    return dp[n]=fun(n-1,dp)+fun(n-2,dp);
 }
-    
-    
-int climbStairs(int n) {
+    int climbStairs(int n) {
         
-        vector<int>dp(n+1,-1);
-         dp[0]=1;
-         dp[1]=1;
-       int ans= steps(n,dp);
+        int dp[n+1];
+        memset(dp,-1,sizeof(dp));
         
-        return ans;
+       return fun(n,dp);
         
     }
 };
