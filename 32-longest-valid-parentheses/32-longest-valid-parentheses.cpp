dd38d81e7fc7d918da59540c32325575b9bcae23
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2022-07-17 05:49:05 +0530

    Time: 9 ms (31.30%), Space: 7.3 MB (66.61%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        int n=s.length();
+        stack<int>st;
+        st.push(-1);
+        int res=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push(i);
+            }
+            
+            else
+            {
+                if(!st.empty())
+                {
+                    st.pop();
+                }
+                
+                if(!st.empty())
+                {
+                    res=max(res,i-st.top());
+                }
+                
+                else
+                {
+                    st.push(i);
+                }
+            }
+        }
+        
+        
+        return res;
+    }
+};
