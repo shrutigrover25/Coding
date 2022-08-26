978bde86a47713029da9220398e1d18684dcf62f
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2022-08-27 03:10:09 +0530

    Time: 10 ms (66.32%), Space: 13.2 MB (47.67%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,102 +1,71 @@
 class Solution {
 public:
-    
-bool isvalid(int x, int y, int n, int m, vector<vector<int>>& grid )
-{
-    if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==1)
-    {
-        return true;
-    }
-    
-    return false;
-}
     int orangesRotting(vector<vector<int>>& grid) {
         
-        int n=grid.size();
-        int m=grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
         
-        int time=0;
-        
-        int fresh=0;
         queue<pair<int,int>>q;
         
-        for(int i=0;i<n;i++)
+        int fresh=0;
+        
+        vector<int>dir={-1,0,1,0,-1};
+        
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==2)
                 {
                     q.push({i,j});
                 }
                 
-                else if(grid[i][j]==1)
+                if(grid[i][j]==1)
                 {
                     fresh++;
                 }
             }
         }
         
-        if(fresh==0)
-        {
-            return 0;
-        }
+        int ans=-1;
         
         while(!q.empty())
         {
             int s=q.size();
-            int temp=0;
             
-            while(s!=0)
+            while(s--)
             {
-                
                 pair<int,int>p=q.front();
                 
                 q.pop();
-                int x1=p.first;
-                int y1=p.second;
-                
-                int arrx[4]={1,-1,0,0};
-                int arry[4]={0,0,1,-1};
                 
                 for(int i=0;i<4;i++)
                 {
-                    int x=arrx[i]+x1;
-                    int y=arry[i]+y1;
+                    int r=p.first+dir[i];
+                    int c=p.second+dir[i+1];
                     
-                    if(isvalid(x,y,n,m,grid))
+                    if(r>=0 && r<m && c>=0 && c<n && grid[r][c]==1)
                     {
-                        temp++;
-                        grid[x][y]=2;
-                        q.push({x,y});
+                        grid[r][c]=2;
+                        q.push({r,c});
+                        fresh--;
                     }
                 }
-                
-                s--;
             }
             
-            if(temp!=0)
-            {
-                time++;
-            }
+            ans++;
         }
         
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]==1)
-                {
-                    time=0;
-                }
-            }
-        }
-        
-        if(time==0)
+        if(fresh>0)
         {
             return -1;
         }
         
-        return time;
+        if(ans==-1)
+        {
+            return 0;
+        }
+        
+        return ans;
     }
 };
