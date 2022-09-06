078a54745728225c7f8be18c269868aaef38be80
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2022-09-06 20:06:38 +0530

    Time: 90 ms (50.16%), Space: 42.1 MB (65.80%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function(n) {
+    
+    let output = new Array(n).fill(0).map(() => new Array(n).fill(0))
+    
+    let count = 0;
+    
+    let size = n * n;
+    
+    let left = 0;
+    
+    let right = n - 1;
+    
+    let top = 0;
+    
+    let bottom = n -1;
+    
+    while(top<=bottom && left<=right){
+        
+        //going left
+        for(let i = left; i <= right; i++){
+            count++;
+            output[top][i] = count;
+        }
+        top++;
+                
+        // going down
+        for(let i = top; i <= bottom; i++){
+            count++;
+            output[i][right] = count;
+        }
+        right--;
+        
+        //going left
+        for(let i = right; i >= left; i--){
+            count++;
+            output[bottom][i] = count;
+        }
+        bottom--;
+        
+        //going up
+        for(let i = bottom; i >= top; i--){
+            count++;
+            output[i][left] = count;
+        }
+        left++;
+    }
+    
+    return output;
+    
+
+    
+};
