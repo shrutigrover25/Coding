eb7485f878687b1c90dd1e68e7137bdedcc8d9c7
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2023-03-09 05:04:10 +0530

    Time: 3 ms (52.11%), Space: 5.7 MB (97.63%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        if(n<=0)
+        {
+            return false;
+        }
+        
+        while(n%2==0)
+        {
+            n=n/2;
+        }
+        
+        if(n==1)
+            return true;
+        
+        
+        return false;
+    }
+};
