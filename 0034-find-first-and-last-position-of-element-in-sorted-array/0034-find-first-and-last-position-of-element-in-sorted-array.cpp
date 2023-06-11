fd352fa3e57d5ca82a819f2bf9298cdadf00b498
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2023-06-11 15:11:21 +0530

    Time: 8 ms (78.01%), Space: 13.7 MB (14.74%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,77 +1,79 @@
 class Solution {
 public:
     
-int firstocc(vector<int>&nums, int n, int target)
-{
-    int low=0;
-    int high=n-1;
-    
-    int ans=-1;
-    
-    while(low<=high)
+    int findfirst(vector<int>&nums, int target, int n)
     {
-        int mid=low+(high-low)/2;
+        int ans=-1;
         
-        if(nums[mid]==target)
+        int low=0;
+        int high=n-1;
+        
+        
+        while(low<=high)
         {
-            ans=mid;
-            high=mid-1;
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            
+            else
+            {
+                low=mid+1;
+            }
         }
         
-        else if(nums[mid]<target)
-        {
-            low=mid+1;
-        }
+        return ans;
         
-        else
-        {
-            high=mid-1;
-        }
     }
     
-    return ans;
-}
     
-    
-int lastocc(vector<int>&nums, int n, int target)
-{
-    int low=0;
-    int high=n-1;
-    
-    int ans=-1;
-    
-    while(low<=high)
+    int findlast(vector<int>&nums, int target, int n)
     {
-        int mid=low+(high-low)/2;
+        int res=-1;
         
-        if(nums[mid]==target)
+        int low=0;
+        int high=n-1;
+        
+       
+        while(low<=high)
         {
-            ans=mid;
-            low=mid+1;
+            
+             int mid=low+(high-low)/2;
+            
+            if(nums[mid]==target)
+            {
+                res=mid;
+                low=mid+1;
+            }
+            
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            
+            else
+            {
+                low=mid+1;
+            }
         }
         
-        else if(nums[mid]<target)
-        {
-            low=mid+1;
-        }
-        
-        else
-        {
-            high=mid-1;
-        }
+        return res;
     }
-    
-    return ans;
-}
     vector<int> searchRange(vector<int>& nums, int target) {
         
         vector<int>v;
-        
         int n=nums.size();
         
-       
-        int ans=firstocc(nums,n,target);
-        int res=lastocc(nums,n,target);
+        int ans=findfirst(nums,target,n);
+        int res=findlast(nums,target,n);
         
         v.push_back(ans);
         v.push_back(res);
