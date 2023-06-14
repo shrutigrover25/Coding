a3edcac8fe6bb8d3bf01e154935108a7e26204a2
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2023-06-14 19:53:40 +0530

    Time: 113 ms (12.28%), Space: 55.4 MB (51.18%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -2,16 +2,30 @@ class Solution {
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
         
-        int n=arr.size();
         int i=0;
         int j=0;
-        int count=0;
+        int n=arr.size();
         
         int sum=0;
+        int count=0;
         
-        for(;i<k;i++)
+        
+        for(i=0;i<k;i++)
         {
             sum+=arr[i];
+        }
+        
+        while(i<n)
+        {
+            if(sum/k>=threshold)
+            {
+                count++;
+            }
+            
+            sum+=arr[i];
+            sum-=arr[j];
+            i++;
+            j++;
         }
         
         if(sum/k>=threshold)
@@ -19,21 +33,11 @@ public:
             count++;
         }
         
-        while(i<n)
-        {
-            sum-=arr[j];
-            sum+=arr[i];
-            
-            i++;
-            j++;
-            
-            if(sum/k>=threshold)
-            {
-                count++;
-            }
-        }
-        
         return count;
+        
+        
+        
+        
         
     }
 };
