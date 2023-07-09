29021b775404f1f21e997d41798cdac2756da7a7
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2023-07-09 17:00:03 +0530

    Time: 84 ms (6.08%), Space: 45.6 MB (5.00%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.js
+++ 2129-capitalize-the-title/2129-capitalize-the-title.js
@@ -6,54 +6,50 @@ var capitalizeTitle = function(title) {
     
     // 1. Form the output string by converting all input characters into lowercase character array.
     
-    const result = title.toLowerCase().split('');
+    const result=title.toLowerCase().split('');
     console.log(result);
-  
+     
     // 2. Initialise both pointers left and right as 0
     
     let left=0;
     let right=0;
-    
+
    
     //3. Run the outer loop over the left pointer by jumping over words by moving left=right+1 till the strings length
     
     for(;left<title.length;left=right+1)
-    {
+        {
             
-        
-    
-    
-    
+
           // 4. Run the inner loop over the right pointer by right++ till the    point we get a space character or we reach string's length
-        
+            
+            
         for(right=left;right<title.length;right++)
             {
-              if(result[right]===" ")
-                  {
-                      break;
-                  }
+               
+                if(result[right]===" ")
+                    {
+                        break;
+                    }
+                
             }
-            
-          
-            
-           
        
          //5. if the word length [right-left>=3] make the left letter of the word   as uppercase
         
-        if(right-left>=3)
-            {
-                result[left]=result[left].toUpperCase();
-            }
-            
-    }
+            if(right-left>=3)
+                {
+                    result[left]=result[left].toUpperCase();
+                }
+      
     
-    
+        }
            
             
     //6. Return the output array in the form of the string
     
     return result.join('');
     
+  
    
     
   
