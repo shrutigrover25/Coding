8e2eedb29e892791c68032d985ab3044728a9d61
Shruti Grover <84871565+shrutigrover25@users.noreply.github.com>
2023-07-09 11:44:34 +0530

    Time: 68 ms (28.90%), Space: 42.6 MB (46.54%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.js
+++ 2129-capitalize-the-title/2129-capitalize-the-title.js
@@ -7,26 +7,25 @@ var capitalizeTitle = function(title) {
     // 1. Form the output string by converting all input characters into lowercase character array.
     
     const result=title.toLowerCase().split('');
-  
+ 
   
     // 2. Initialise both pointers left and right as 0
     
     let left=0;
     let right=0;
-     
    
-    
-    
     //3. Run the outer loop over the left pointer by jumping over words by moving left=right+1 till the strings length
     
     for(;left<title.length;left=right+1)
         {
+            
+ 
  
     // 4. Run the inner loop over the right pointer by right++ till the point we get a space character or we reach string's length
             
             for(right=left;right<title.length;right++)
                 {
-                    if(result[right]=== ' ')
+                    if(result[right]===" ")
                         {
                             break;
                         }
@@ -40,10 +39,13 @@ var capitalizeTitle = function(title) {
                 {
                     result[left]=result[left].toUpperCase();
                 }
-        }
+        }    
+            
     //6. Return the output array in the form of the string
     
     return result.join('');
+    
+    
     
   
 };
